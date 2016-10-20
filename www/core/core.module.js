2c23fd3ff3ed2a8dc11d5885c46ef1e3fc88b153
Drumuch <bichvld@gmail.com>
2016-10-20 17:40:14 +0300

    try

---
+++ www/core/core.module.js
@@ -0,0 +1,19 @@
+;
+(function() {
+    'use strict';
+    angular
+        .module('app.core', [
+        /*
+         * Angular modules
+         */
+
+        /*
+         * Our reusable cross app code modules
+         */
+
+        /*
+         * 3rd Party modules
+         */
+        'ionic', 'starter.controllers'
+        ])
+})();
