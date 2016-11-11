bf522dc528eb8cc6ab5ce5a3b4ebbe5a3b00a046
Inceon <mr.inceon@gmail.com>
2016-11-11 13:24:41 +0200

    fix reload themes list in section

--- www/views/sections/sections.controller.js
+++ www/views/sections/sections.controller.js
@@ -19,13 +19,14 @@
         vm.sections = allSections.section.models;
 
         function showThemes(section) {
-            if (section.data) {
-                delete section.data;
-            } else {
+            if (!section.data) {
                 book.themes(section.id)
                     .then(function (res) {
                         section.data = res.models;
+                        section.data.show = true;
                     });
+            } else {
+                section.data.show = !section.data.show;
             }
         }
     }
