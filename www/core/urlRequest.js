0da580a9506c8013ecddbe1e9555c37560c74a9f
Inceon <mr.inceon@gmail.com>
2016-11-23 16:05:41 +0200

    add chat to task page and one form for answer and question

--- www/core/urlRequest.js
+++ www/core/urlRequest.js
@@ -7,7 +7,7 @@
 
                 // var baseUrl = 'http://192.168.0.112/api/web/v1/';
                 // var baseUrl = 'http://192.168.0.118/api/web/v1/';
-                // var baseUrl = 'http://192.168.0.107/api/web/v2/';
+                // var baseUrl = 'http://192.168.0.105/api/web/v2/';
                 // var baseUrl = 'http://school-maths/api/web/v1/';
                 var baseUrl = 'http://school.apes-at-work.com/api/web/v2/';
                 return {
--- www/views/task/one/task.one.controller.js
+++ www/views/task/one/task.one.controller.js
@@ -21,7 +21,21 @@
         vm.task = taskInfo;
         vm.upload = upload;
         vm.question = question;
-        vm.ask = ask;
+        vm.changeForm = changeForm;
+        vm.askForm = false;
+        vm.messages = [
+            {id:1, text:"Did you get my message, the one I left"},
+            {id:1, text:"While I was trying to convince everything"},
+            {id:2, text:"That I meant in a minute or less when I called to confess"},
+            {id:1, text:"And make all of my stresses go bye-bye"},
+            {id:2, text:"Did you get my message, you did not guess"},
+            {id:1, text:"Cause if you did you would have called me with your sweet intent"},
+            {id:2, text:"And we could give it a rest"},
+            {id:2, text:"stead of beating my breast"},
+            {id:1, text:"Making all of the pressure go sky-high"},
+            {id:1, text:"Do you ever wonder what happens to the words that we send"},
+            {id:2, text:"Do they bend, do they break from the flight that they take"},
+        ];
 
         vm.data = vm.task.done || {};
         if (vm.task.done) {
@@ -74,8 +88,9 @@
             });
         }
 
-        function ask() {
-            alert();
+        function changeForm() {
+            vm.askForm = !vm.askForm;
+            vm.label.result = vm.askForm ? "Задати питання" : "Відповідь";
         }
 
     }
